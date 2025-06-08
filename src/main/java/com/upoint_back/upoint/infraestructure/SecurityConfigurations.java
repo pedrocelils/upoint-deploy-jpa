a053f2738211b7421d980a20fbd569bd2ecc1ecf
Pedro Célio <95891819+pedrocelils@users.noreply.github.com>
2025-06-08 02:19:42 -0300

    Update SecurityConfigurations.java
    alterando a url do app

--- src/main/java/com/upoint_back/upoint/infraestructure/SecurityConfigurations.java
+++ src/main/java/com/upoint_back/upoint/infraestructure/SecurityConfigurations.java
@@ -99,7 +99,7 @@ public class SecurityConfigurations {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8081/", "http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:8081", "http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
